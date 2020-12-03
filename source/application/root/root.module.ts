4495418de74ff881b74b243775ce215cdd31e031
hotequil <joaohotequil@gmail.com>
2020-12-02 21:38:17 -0300

    ajustado arquitetura

---
+++ source/application/root/root.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RoutesModule } from '../routes/routes.module';
+import { RootComponent } from './root.component';
+
+@NgModule({
+    declarations: [
+        RootComponent
+    ],
+    imports: [
+        BrowserModule,
+        RoutesModule
+    ],
+    providers: [],
+    bootstrap: [
+        RootComponent
+    ]
+})
+export class RootModule{}
