2031a4d142e0ffd3d35c04e38f2e8a2dd034a15d
hotequil <joaohotequil@gmail.com>
2020-12-05 14:59:07 -0300

    footer feito.

--- source/application/root/root.module.ts
+++ source/application/root/root.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { RoutesModule } from '../routes/routes.module';
 import { RootComponent } from './root.component';
+import { HomeModule } from '../home/home.module';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
     declarations: [
@@ -10,7 +12,9 @@ import { RootComponent } from './root.component';
     ],
     imports: [
         BrowserModule,
-        RoutesModule
+        RoutesModule,
+        HomeModule,
+        FooterModule
     ],
     providers: [],
     bootstrap: [
