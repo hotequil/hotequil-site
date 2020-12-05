3e0a9ac8941d9b6a895f0d91db95b5142b141e44
hotequil <joaohotequil@gmail.com>
2020-12-05 13:12:55 -0300

    ajustado favicon

--- source/application/root/root.component.ts
+++ source/application/root/root.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'hotequil-root',
     templateUrl: './root.component.html',
     styleUrls: ['./root.component.scss']
 })
-export class RootComponent {
-    title = 'hotequil';
+export class RootComponent implements OnInit{
+    ngOnInit(): void{
+        console.log('A verdadeira utopia está aí, vários prédios caindo aos pedaços, educação centralizada, documentos carimbados, falsos contratos, pagamentos involuntários, papeis positivados e apelo ao fino, até pelo que eu sei, você não nasceu com isso.');
+    }
 }
