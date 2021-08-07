9e8fa94eced50a7195edbb3f3ae0bbd632c12cc1
hotequil <joaohotequil@gmail.com>
2021-08-07 18:43:05 -0300

    Feito testes do componente do header.

--- src/app/modules/header/header.component.spec.ts
+++ src/app/modules/header/header.component.spec.ts
@@ -1,25 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { HeaderModule } from './header.module';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({ imports: [HeaderModule] }).compileComponents();
+
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('Should create the component when starting app', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Should show logo when 1000ms when onInit was called', done => {
+        fixture.detectChanges();
+
+        setTimeout(() => {
+            expect(component.showLogoImg).toBeTrue();
+
+            done();
+        }, 1000);
     })
-    .compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('Should has HTML Element when onInit was called', () => {
+        fixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(component.htmlEl?.tagName === "HTML").toBeTrue();
+    });
+
+    it('Should show buttons when onInit was called', () => {
+        fixture.detectChanges();
+
+        expect(component.showBtns).toBeTrue();
+    });
 });
