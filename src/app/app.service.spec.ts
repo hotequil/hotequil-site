eac4db656c66727a59da94a2bee6809dd6c9bef0
hotequil <joaohotequil@gmail.com>
2021-08-07 17:16:28 -0300

    Feito testes do serviço do app.

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -2,15 +2,18 @@ import { TestBed } from '@angular/core/testing';
 
 import { AppService } from './app.service';
 
-describe('AppService', () => {
-  let service: AppService;
+describe(AppService.name, () => {
+    let service: AppService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AppService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = TestBed.inject(AppService);
+    });
+
+    it(`isBrowser should return a boolean when was called`, () => {
+        const value = service.isBrowser;
+
+        expect(typeof(value) === 'boolean').toBeTrue();
+    });
 });
