5b8880626adc316802dd431b1494f22d9a2ec003
hotequil <hotequil@protonmail.com>
2025-03-10 22:12:10 -0300

    feat: remove home

--- src/app/modules/home/message/home-message.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'h-home-message',
-    templateUrl: './home-message.component.html',
-    styleUrls: ['./home-message.component.scss']
-})
-export class HomeMessageComponent{}
--- src/app/modules/home/projects/home-projects.ts
+++
@@ -1,8 +0,0 @@
-import { Img } from '../../../shared/models/img/img';
-
-export interface HomeProjects {
-    title: string;
-    desc: string;
-    href: string;
-    img: Img;
-}
